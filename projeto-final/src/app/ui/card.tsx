2e21b5df41bc2ab73722b465c653251bbc81a402
Digao-Onofri <digao@Digao.localdomain>
2025-06-26 16:57:44 -0300

    initial things to make the CRUD work properly

--- projeto-final/src/app/ui/card.tsx
+++ projeto-final/src/app/ui/card.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import styles from "@/app/styles/card.module.css"
+import Link from "next/link";
 
 
 // Criando e definindo o tipo do Card, que sempre terá esse formato
@@ -13,6 +14,11 @@ export interface CarrosProps{
 }
 
 export default function Card(props: CarrosProps){
+
+    const deleteCarro = async () => {
+        'use server';
+    }
+
     return(
         <div className={styles.card}>
             <h2>{props.nome}</h2>
@@ -23,6 +29,12 @@ export default function Card(props: CarrosProps){
                 alt={`Imagem do carro ${props.nome}`}
             />
             <p>{props.descricao}</p>
+            <div className="carros-buttons-container">
+                <Link href={`/dashboard/edit ${props.id}`} id="btn-edit">Editar</Link>
+                <form action={deleteCarro}>
+                    <button id="btn-delete">Deletar</button>
+                </form>
+            </div>
         </div>
     );
 }
--- projeto-final/src/app/ui/formCadastro.tsx
+++ projeto-final/src/app/ui/formCadastro.tsx
@@ -5,7 +5,7 @@ import styles from "@/app/styles/formCadastro.module.css"
 export default function Cadastro(){
     return(
         <div className={styles.div}>
-            <form action="" className={styles.cadastro}>
+            <form action='' className={styles.cadastro}>
                 <h1>Cadastro</h1>
                 <input type="text" placeholder="Nome"/>
                 <input type="password" placeholder="Senha"/>
