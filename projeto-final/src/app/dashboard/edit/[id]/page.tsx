2f74cad3437887d8e741d7f70d13ddb9fa865e3f
Digao-Onofri <digao@Digao.localdomain>
2025-06-26 22:17:26 -0300

    edit of the CRUD already working

--- projeto-final/src/app/dashboard/edit/[id]/page.tsx
+++ projeto-final/src/app/dashboard/edit/[id]/page.tsx
@@ -1,16 +1,61 @@
+// Componente form de edicao de carros
 import Menu from "@/app/ui/menu"
 import ConexaoBD from "@/app/libs/conexao-bd";
 import styles from "@/app/styles/paginas.module.css";
-import Form from "@/app/ui/formEditCarro";
-export default function Creater(){
-    return(
+import styles2 from "@/app/styles/formCadCarro.module.css";
+import { CarrosProps } from "@/app/ui/card";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const arquivo = 'carros-db.json';
+
+interface EditCarroProps{
+    params: Promise<{id: string}>;
+}
+
+export default async function editCarro({params}: EditCarroProps) {
+
+    const {id} = await params;
+    
+    const carroDB = await ConexaoBD.retornaBD(arquivo);
+    const carroToEdit : CarrosProps = carroDB.find((c : CarrosProps) => c.id === id);
+    const carroToEditIndex : number = carroDB.findIndex((c) => c.id === id);
+
+    const updateCarro = async (formData : FormData) => { 
+        'use server';
+        const updateCarro : CarrosProps ={
+            id, 
+            nome: formData.get('nome') as string,
+            descricao: formData.get('descricao') as string,
+            img: formData.get('img') as string
+        }
+        carroDB.splice(carroToEditIndex, 1, updateCarro);
+        await ConexaoBD.armazenaBD(arquivo, carroDB);
+        redirect('/main/inicio');
+    }
+
+    return (
         <>
-        <div className={styles.menu}>
-            <Menu />
-        </div>
-        <div className={styles.conteudo}>
-            <Form />
-        </div>
-        </>
+            <div className={styles.menu}>
+                <Menu />
+            </div>
+            <div className={styles.conteudo}>
+                <div className={styles2.div}>
+                    <form action={updateCarro} className={styles2.form}>
+                        <h1>{carroToEdit.nome}</h1>                    
+                        <Image src={carroToEdit.img}    
+                            alt={carroToEdit.nome}
+                            width={100}
+                            height={100}
+                            style={{margin: "0 auto"}}
+                        />
+                        <input id="nome" name="nome" type="text" placeholder="Nome" defaultValue={carroToEdit.nome}/>
+                        <input id="img" name="img" type="text" placeholder="Link da imagem" defaultValue={carroToEdit.img} />
+                        <input id="descricao" name="descricao" type="text" placeholder="Descrição" defaultValue={carroToEdit.descricao} />
+                        <button type="submit"><a href="/main/inicio">Atualizar</a></button>
+                    </form>
+                </div>
+            </div>
+        </>        
     );
 }
