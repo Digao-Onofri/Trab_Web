277168a0a0f1187e19daa0c005fce64507b3ffc2
Digao-Onofri <digao@Digao.localdomain>
2025-07-01 18:38:44 -0300

    preorganize the file list/page.tsx for list all the cars

--- projeto-final/src/app/main/list/page.tsx
+++ projeto-final/src/app/main/list/page.tsx
@@ -1,5 +1,7 @@
 import Menu from "@/app/ui/menu"
 import styles from "@/app/styles/paginas.module.css"
+import CarrosPadrao from "@/db/carros-db.json"
+import Card from "@/app/ui/card"
 
 export default function List(){
     return(
@@ -7,9 +9,25 @@ export default function List(){
         <div className={styles.menu}>
             <Menu />
         </div>
+
         <div className={styles.conteudo}>
+            <div className={styles.destaques}>
             
+            <h3>Carros em Destaque</h3>
+                <div className={styles.card}>
+                    {CarrosPadrao.map(carro => (
+                        <Card 
+                            key={carro.id}
+                            id={carro.id}
+                            nome={carro.nome}
+                            img={carro.img}
+                            descricao={carro.descricao}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
+
         </>
     );
 }
