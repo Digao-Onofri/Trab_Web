753106959b2f3aa28097e40fb06796af6f240a52
Pedro Luiz <pedroluiz2070@gmail.com>
2025-07-04 19:34:22 -0300

    feat: styling the listing screen and arranging the car registration screen

--- projeto-final/src/app/main/list/page.tsx
+++ projeto-final/src/app/main/list/page.tsx
@@ -1,5 +1,6 @@
 import Menu from "@/app/ui/menu"
 import styles from "@/app/styles/paginas.module.css"
+import stylesList from "@/app/styles/list.module.css"
 import CarrosPadrao from "@/db/carros-db.json"
 import Card from "@/app/ui/card"
 
@@ -11,20 +12,21 @@ export default function List(){
         </div>
 
         <div className={styles.conteudo}>
-            <div className={styles.destaques}>
-            
-            <h3>Carros em Destaque</h3>
-                <div className={styles.card}>
-                    {CarrosPadrao.map(carro => (
-                        <Card 
-                            key={carro.id}
-                            id={carro.id}
-                            nome={carro.nome}
-                            img={carro.img}
-                            descricao={carro.descricao}
-                        />
-                    ))}
-                </div>
+            <div className={stylesList.destaques}>
+                <div className={stylesList.conteudoList}>
+                    <h3 className={stylesList.h3}>Todos os seus carros</h3>
+                    <div className={stylesList.card}>
+                        {CarrosPadrao.map(carro => (
+                            <Card 
+                                key={carro.id}
+                                id={carro.id}
+                                nome={carro.nome}
+                                img={carro.img}
+                                descricao={carro.descricao}
+                            />
+                        ))}
+                    </div>
+                </div>      
             </div>
         </div>
 
