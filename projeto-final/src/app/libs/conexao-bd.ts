d5cc9c974588082948d61ead01f9fdb8cfbe2b0f
Digao-Onofri <goncalves.onofri@gmail.com>
2025-07-18 12:35:26 -0300

    adding comments to solve the problem of passing any as params

--- projeto-final/src/app/libs/conexao-bd.ts
+++ projeto-final/src/app/libs/conexao-bd.ts
@@ -2,6 +2,7 @@
 import {promises as fs} from 'fs';
 import path from "path";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function retornaBD(arquivo: string): Promise<Array<any>>
 {
     const dbPath = path.join(process.cwd(),'src', 'app','db',arquivo);
@@ -10,6 +11,7 @@ async function retornaBD(arquivo: string): Promise<Array<any>>
     return JSON.parse(dados);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function armazenaBD(arquivo: string, dados: any)
 {
     const dbPath = path.join(process.cwd(),'src', 'app','db',arquivo);
